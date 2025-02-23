bcad2b6f80b5ba4f45003ead08122f06ee9df2ba
Xuehan Jing <jingxuehan0707@gmial.com>
2025-02-23 14:20:52 -0800

    feat: agent interacts with hazard

--- cell.py
+++ cell.py
@@ -2,7 +2,7 @@ import mesa
 import mesa_geo as mg
 import rasterio as rio
 
-class FireHazard(mg.Cell):
+class FireHazardCell(mg.Cell):
     def __init__(self, model, pos, indices):
         super().__init__(model, pos, indices)
         self.fire_arrival_time = None  # in minutes
@@ -10,6 +10,6 @@ class FireHazard(mg.Cell):
 
     def step(self):
 
-        if (self.model.time_elapsed >= self.fire_arrival_time * 60) and (self.is_burnt == False):
+        if (self.model.time_elapsed >= self.fire_arrival_time * 60) and (self.is_burnt == False) and (self.fire_arrival_time != -9999):
             self.is_burnt = True
         
--- model.py
+++ model.py
@@ -1,12 +1,14 @@
 import mesa
 import mesa_geo as mg
-from shapely.geometry import Point
+from shapely.geometry import Point, Polygon, shape
 import geopandas as gpd
-from agents import Resident, Shelter
+from agents import Resident, Shelter, FireHazard
 from road_network import RoadNetwork
 from space import StudyArea
-from cell import FireHazard
+from cell import FireHazardCell
 import rasterio as rio
+from rasterio.transform import Affine
+import rasterio.features
 import numpy as np
 
 # Note: When using mg.AgentCreator, the agents are stored in the self.model
@@ -25,13 +27,26 @@ class EvacuationModel(mesa.Model):
 
     # The hazard raster path
     hazard_raster = "data/pcs/fire_arrival_time.asc"
+    hazard_raster_translated = "data/pcs/fire_arrival_time_translated.asc"
+
+    # Create a new raster with affine transform (-100, 0)
+    with rio.open(hazard_raster) as src:
+        data = src.read(1)
+        transform = src.transform * Affine.translation(-20, 20)
+        meta = src.meta.copy()
+        meta.update({"transform": transform})
+        
+        with rio.open(hazard_raster_translated, 'w', **meta) as dst:
+            dst.write(data, 1)
+        
+
 
     # Create gdf for the shapefile
     population_distribution_gdf = gpd.read_file(population_distribution_shp)
     shelters_gdf = gpd.read_file(shelters_shp)
     road_network_gdf = gpd.read_file(road_network_shp)
 
-    def __init__(self, num_steps=100, num_residents=100):
+    def __init__(self, num_steps=100, num_residents=1):
         super().__init__()
         self.space = StudyArea(crs="EPSG:32611",warn_crs_conversion=True)
         self.road_network = RoadNetwork(geo_series=self.road_network_gdf['geometry'], use_cache=True)
@@ -62,14 +77,25 @@ class EvacuationModel(mesa.Model):
         self.space.add_agents(resident_agents)
 
         # Create fire hazard cells
-        with rio.open(self.hazard_raster) as src:
-            values = src.read()
+        with rio.open(self.hazard_raster_translated) as src:  
+            values = src.read()      
             width = src.width
             height = src.height
+            transform = src.transform            
             bounds = list(src.bounds)
-            hazard_raster_layer = mg.RasterLayer(width, height, "EPSG:32611", bounds, self, FireHazard)
+            hazard_raster_layer = mg.RasterLayer(width, height, "EPSG:32611", bounds, self, FireHazardCell)
             hazard_raster_layer.apply_raster(values, "fire_arrival_time")
+            hazard_raster_layer.apply_raster(np.zeros_like(values), "is_burnt")
             self.space.add_layer(hazard_raster_layer)
+
+        # Creare the fire hazard geoagent
+        mask = values > 0
+        hazard_shapes = list(rasterio.features.shapes(values, mask=mask, transform=transform))
+        hazard_geoms = [shape(geom) for geom, value in hazard_shapes if value >= 0]
+        hazard_gdf = gpd.GeoDataFrame(geometry=hazard_geoms, crs="EPSG:32611")
+        hazard_ag_creator = mg.AgentCreator(FireHazard, model=self)
+        hazard_agents = hazard_ag_creator.from_GeoDataFrame(hazard_gdf.dissolve())
+        # self.space.add_agents(hazard_agents)
             
         # Data collector
         self.datacollector = mesa.DataCollector(
@@ -83,9 +109,10 @@ class EvacuationModel(mesa.Model):
     def step(self):
 
         self.time_elapsed = self.steps * self.step_interval
-
-        self.agents_by_type[Resident].do("step")
+    
+        self.agents_by_type[FireHazardCell].do("step")
         self.agents_by_type[FireHazard].do("step")
+        self.agents_by_type[Resident].do("step")
 
         self.datacollector.collect(self)
 
@@ -105,7 +132,7 @@ def get_count_agent_sheltered(model):
 
 def demo():
     model = EvacuationModel()
-    for i in range(2):
+    for i in range(3):
         model.step()
         get_count_agent_sheltered(model)
 
